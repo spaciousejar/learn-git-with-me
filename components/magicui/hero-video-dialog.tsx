733b62ee92f8d6de3e0088e782b8260adf645547
spaciousejar <jadhavvicky752@gmail.com>
2024-10-14 11:52:44 +0530

    FIX PRODUCTION bugs

--- components/magicui/hero-video-dialog.tsx
+++ components/magicui/hero-video-dialog.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Play, XIcon } from "lucide-react";
+import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 
@@ -83,7 +84,7 @@ export default function HeroVideoDialog({
         className="relative cursor-pointer group"
         onClick={() => setIsVideoOpen(true)}
       >
-        <img
+        <Image
           src={thumbnailSrc}
           alt={thumbnailAlt}
           width={1920}
