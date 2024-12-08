c845b212cbaa8ee36267a1fb30fcaef660e8399f
spaciousejar <jadhavvicky752@gmail.com>
2024-12-09 04:50:40 +0530

    Update routes-config to clarify the purpose of the noLink property in EachRoute type, enhancing code readability.

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -3,7 +3,7 @@
 export type EachRoute = {
   title: string;
   href: string;
-  noLink?: true;
+  noLink?: true; // noLink will create a route segment (section) but cannot be navigated
   items?: EachRoute[];
 };
 
