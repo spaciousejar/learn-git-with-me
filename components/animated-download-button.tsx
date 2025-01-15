22130f5ae0a7daa6dc5a6f93e4c9edb849133898
spaciousejar <jadhavvicky752@gmail.com>
2025-01-16 04:31:48 +0530

    Refactor DownloadButton component for improved readability
    - Removed unnecessary state management for hover effects in the DownloadButton component.
    - Streamlined the button structure while maintaining functionality for downloading the cheatsheet and displaying a toast notification upon click.

--- components/animated-download-button.tsx
+++ components/animated-download-button.tsx
@@ -1,41 +1,29 @@
 'use client'
 
-import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Download } from "lucide-react"
 import { toast } from "@/hooks/use-toast"
 
 export function DownloadButton() {
-  const [isHovered, setIsHovered] = useState(false)
-
   return (
-   <Button
-   variant="ghost">
-     <a
-      className="flex"
-      href="git-cheatsheet.pdf"
-      download="git-cheatsheet.pdf"
-      
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-
-      onClick={() => {
-        toast({
-          title: "Congratulations! 🎉",
-          description: "You have successfully downloaded the CHEATSHEET.",
-        })
-      }}
-
-     >
-      <span className="inset-0 opacity-20 rounded-md animate-pulse"></span>
-      <Download 
-        className={`mr-2 h-4 w-4 transition-transform duration-300 ease-in-out`} 
-      />
-      CHEATSHEET
-      
+    <Button variant="ghost">
+      <a
+        className="flex"
+        href="git-cheatsheet.pdf"
+        download="git-cheatsheet.pdf"
+        onClick={() => {
+          toast({
+            title: "Congratulations! 🎉",
+            description: "You have successfully downloaded the CHEATSHEET.",
+          })
+        }}
+      >
+        <span className="inset-0 opacity-20 rounded-md animate-pulse"></span>
+        <Download 
+          className={`mr-2 h-4 w-4 transition-transform duration-300 ease-in-out`} 
+        />
+        CHEATSHEET
       </a>
-      </Button>
-      
-    
+    </Button>
   )
 }
