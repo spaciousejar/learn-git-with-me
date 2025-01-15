f43c945bf01d489d2401a988c6ebf45cfdb517d3
spaciousejar <jadhavvicky752@gmail.com>
2025-01-16 04:14:35 +0530

    nextjs 15

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants.js";
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = async (phase: string) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = (await import("@ducanh2912/next-pwa")).default({
+      dest: "public",
+    });
+    return withPWA({
+      reactStrictMode: true,
+      images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "img.freepik.com/**",
+          },
+        ],
+      },
+    });
+  }
+};
+
+
+export default nextConfig;
+
+
