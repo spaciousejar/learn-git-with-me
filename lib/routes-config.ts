7abf4b08dc7a6bcb916cb56057998d29a6d1653d
JaiminBariya <jaiminbariyastudent@gmail.com>
2025-01-15 10:39:36 +0530

    Add documentation for .gitignore file

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 // for page navigation & to sort on leftbar
 
 export type EachRoute = {
@@ -8,43 +10,40 @@ export type EachRoute = {
 };
 
 export const ROUTES: EachRoute[] = [
-  
   {
     title: "Getting Started",
     href: "/getting-started",
     noLink: true,
     items: [
-
-      { title: "Introduction",
-         href: "/introduction",
-        
-      },  
+      { title: "Introduction", href: "/introduction" },
       {
         title: "Installation",
         href: "/installation",
-       /* items: [
+        /* items: [
           { title: "Windows", href: "/windows" },
           { title: "Mac", href: "/mac" },
           { title: "Linux", href: "/linux" },
         ],*/
       },
-      
-  ]
-},
-{ 
-  title: "Basic Concepts",
-  href: "/basic-concepts",
-noLink: true,
-  items: [
-    { title: "Why Use git", href: "/why-use-git" },
-    {
-      title: "Tip to Master Git", href: "/tips"},
-  ]},
+    ],
+  },
+  {
+    title: "Basic Concepts",
+    href: "/basic-concepts",
+    noLink: true,
+    items: [
+      { title: "Why Use git", href: "/why-use-git" },
+      {
+        title: "Tip to Master Git",
+        href: "/tips",
+      },
+    ],
+  },
 
-{ 
-  title: "Essential Commands",
-  href: "/essential-commands",
-  noLink: true,
+  {
+    title: "Essential Commands",
+    href: "/essential-commands",
+    noLink: true,
     items: [
       { title: "Configure Git", href: "/configure-git" },
       { title: "Initialize Git", href: "/Initialize-Git-repository" },
@@ -54,53 +53,57 @@ noLink: true,
       { title: "Push changes", href: "/git-push" },
       { title: "Pull changes", href: "/git-pull" },
       { title: "Clone repository", href: "/git-clone" },
-      { title: "Branching",
-         href: "/branching",
-         noLink: true,
-         items: [
+      {
+        title: "Branching",
+        href: "/branching",
+        noLink: true,
+        items: [
           { title: "Create branch", href: "/create-branch" },
           { title: "List branches", href: "/git-branch" },
           { title: "Switch branch", href: "/git-checkout" },
           { title: "Merge branch", href: "/git-merge" },
           { title: "Delete branch", href: "/delete-branch" },
-         ]},
+        ],
+      },
       { title: "View commit history", href: "/git-log" },
       { title: "View changes", href: "/git-diff" },
       { title: "Fetch changes", href: "/git-fetch" },
       { title: "Remote repositories", href: "/git-remote" },
       { title: "Reset changes", href: "/git-reset" },
-      { title: "Revert changes", href: "/git-revert" },     
-    ]
+      { title: "Revert changes", href: "/git-revert" },
+    ],
+  },
+  {
+    title: "File Management in Git",
+    href: "/file-management-in-git",
+    noLink: true,
+    items: [{ title: "The .gitignore File", href: "/the-gitignore-file" }],
+  },
 
-},
+  // {
+  //   title: "Advanced Commands",
+  //   href: "/advanced-commands",
+  //   noLink: true,
+  //   items: [
+  //     { title: "Cherry-pick", href: "/git-cherry-pick" },
+  //     { title: "Rebase", href: "/git-rebase" },
+  //     { title: "Squash commits", href: "/git-squash" },
+  //     { title: "Interactive rebase", href: "/git-interactive-rebase" },
+  //     { title: "Submodules", href: "/git-submodules" },
+  //     { title: "Bisect", href: "/git-bisect" },
+  //     { title: "Reflog", href: "/git-reflog" },
+  //     { title: "Worktrees", href: "/git-worktrees" },
+  //     { title: "Hooks", href: "/git-hooks" },
+  //     { title: "Tags", href: "/git-tags" },
+  //     { title: "Stash", href: "/git-stash" },
+  //     { title: "Blame", href: "/git-blame" },
+  //     { title: "Stash changes", href: "/git-stash" },
 
-/*
-{
-  title: "Advanced Commands",
-  href: "/advanced-commands",
-  noLink: true,
-  items: [
-    { title: "Cherry-pick", href: "/git-cherry-pick" },
-    { title: "Rebase", href: "/git-rebase" },
-    { title: "Squash commits", href: "/git-squash" },
-    { title: "Interactive rebase", href: "/git-interactive-rebase" },
-    { title: "Submodules", href: "/git-submodules" },
-    { title: "Bisect", href: "/git-bisect" },
-    { title: "Reflog", href: "/git-reflog" },
-    { title: "Worktrees", href: "/git-worktrees" },
-    { title: "Hooks", href: "/git-hooks" },
-    { title: "Tags", href: "/git-tags" },
-    { title: "Stash", href: "/git-stash" },
-    { title: "Blame", href: "/git-blame" },
-    { title: "Stash changes", href: "/git-stash" },
-      
-    { title: "Undo changes", href: "/git-restore" },
-    { title: "Remove files", href: "/git-rm" },
-    { title: "Move files", href: "/git-mv" },
-   ]
-}
-*/
-    
+  //     { title: "Undo changes", href: "/git-restore" },
+  //     { title: "Remove files", href: "/git-rm" },
+  //     { title: "Move files", href: "/git-mv" }
+  //   ],
+  // },
 ];
 
 type Page = { title: string; href: string };
